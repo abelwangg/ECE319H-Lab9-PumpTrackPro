037eb44ff94f9eb81e481be9fc35905db69c8b52
lolmaomg <alex6010111@gmail.com>
2026-04-17 23:50:59 -0500

    implement sound playback

--- Sound.cpp
+++ Sound.cpp
@@ -8,25 +8,52 @@
 #include "Sound.h"
 #include "sounds/sounds.h"
 #include "../inc/DAC5.h"
-#include "../inc/Timer.h"
+#include "../inc/Clock.h"
 
+static const uint8_t *volatile SoundPt = nullptr;
+static volatile uint32_t SoundCount = 0;
+
+enum : uint32_t{
+  SOUND_PRIORITY = 1,
+  SOUND_SAMPLE_RATE = 11025,
+  DAC_SILENCE = 16
+};
 
 
 void SysTick_IntArm(uint32_t period, uint32_t priority){
-  // write this
+  SysTick->CTRL  = 0x00;      // disable during initialization
+  SysTick->LOAD  = period-1;  // set reload register
+  SCB->SHP[1]    = (SCB->SHP[1]&(~0xC0000000))|(priority<<30);
+  SysTick->VAL   = 0;         // clear count, cause reload
+  SysTick->CTRL  = 0x07;      // enable SysTick with interrupts
 }
 // initialize a 11kHz SysTick, however no sound should be started
 // initialize any global variables
 // Initialize the 5 bit DAC
 void Sound_Init(void){
-// write this
- 
+  DAC5_Init();
+  DAC5_Out(DAC_SILENCE);
+  SoundPt = nullptr;
+  SoundCount = 0;
+  uint32_t period = Clock_Freq()/SOUND_SAMPLE_RATE;
+  if(period == 0){
+    period = 1;
+  }
+  SysTick_IntArm(period, SOUND_PRIORITY);
+  SysTick->CTRL = 0x00; // armed, but playback starts in Sound_Start()
 }
 extern "C" void SysTick_Handler(void);
 void SysTick_Handler(void){ // called at 11 kHz
-  // output one value to DAC if a sound is active
-    // output one value to DAC if a sound is active
-
+  if(SoundCount && SoundPt){
+    DAC5_Out((*SoundPt)>>3); // convert 8-bit sample to 5-bit DAC output
+    SoundPt++;
+    SoundCount--;
+  }else{
+    DAC5_Out(DAC_SILENCE);
+    SysTick->CTRL = 0x00;
+    SoundPt = nullptr;
+    SoundCount = 0;
+  }
 }
 
 //******* Sound_Start ************
@@ -40,35 +67,43 @@ void SysTick_Handler(void){ // called at 11 kHz
 // Output: none
 // special cases: as you wish to implement
 void Sound_Start(const uint8_t *pt, uint32_t count){
-// write this
-  
+  __disable_irq();
+  if((pt == nullptr) || (count == 0)){
+    SoundPt = nullptr;
+    SoundCount = 0;
+    DAC5_Out(DAC_SILENCE);
+    SysTick->CTRL = 0x00;
+  }else{
+    SoundPt = pt;
+    SoundCount = count;
+    SysTick->VAL = 0;
+    SysTick->CTRL = 0x07;
+  }
+  __enable_irq();
 }
 
 void Sound_Shoot(void){
-// write this
   Sound_Start( shoot, 4080);
 }
 void Sound_Killed(void){
-// write this
-
+  Sound_Start(invaderkilled, 3377);
 }
 void Sound_Explosion(void){
-// write this
-
+  Sound_Start(explosion, 2000);
 }
 
 void Sound_Fastinvader1(void){
-
+  Sound_Start(fastinvader1, 982);
 }
 void Sound_Fastinvader2(void){
-
+  Sound_Start(fastinvader2, 1042);
 }
 void Sound_Fastinvader3(void){
-
+  Sound_Start(fastinvader3, 1054);
 }
 void Sound_Fastinvader4(void){
-
+  Sound_Start(fastinvader4, 1098);
 }
 void Sound_Highpitch(void){
-
+  Sound_Start(highpitch, 1802);
 }
